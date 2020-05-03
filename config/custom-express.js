f1312a194d0f7abd275f04462529fcf7d0a9ea7f
walacefex <walacefex01@gmail.com>
2020-05-02 23:05:57 -0300

    Criando mais uma variavel demais uma dependecia, body-parser edepois chamando ela

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let consign = require('consign');
+let bodyParser = require('body-parser');
 
 module.exports  = () =>{
     let app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json());
+
+
     consign()
         .include('./controllers')
         .into(app);
