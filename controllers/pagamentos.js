b98b692dd3ffb11e9d06f8e4d75bb5682ac8112a
walacefex <walacefex01@gmail.com>
2020-05-02 23:06:45 -0300

    criando mais uma rota para o pagamento via post

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,5 +4,9 @@ module.exports = (app) =>{
         console.log('Recebida requisicao de teste na porta 3000');
         res.send('OK');
     });
-
+    app.post('/pagamentos/pagamento', (req, res) =>{
+        let pagamento = req.body;
+        console.log(pagamento)
+        res.send('OK')
+    } )
 }
